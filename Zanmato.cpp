4a5237fdf2aac92b2aa59419dd70dba18b9ece55
dogukans1 <44346685+dogukans1@users.noreply.github.com>
2023-08-03 09:38:35 +0300

    Update Zanmato.cpp
    Optimized by ChatGPT lol

--- Zanmato.cpp
+++ Zanmato.cpp
@@ -1,63 +1,71 @@
-﻿#include<iostream>
-#include"GilMotivation.h" //calcuation of Yojimbo's pay
-#include"ThirdCalcuation.h"
+#include <iostream>
+#include "GilMotivation.h"
+#include "ThirdCalcuation.h"
+
+// Constants
+const int MIN_YC = 0;
+const int MAX_YC = 255;
+const int MIN_GIL = 1;
+const int MAX_GIL = 999999999;
+const int MAX_ZL = 3;
+const int MIN_ZL = 1;
+const int ZANMATO_GUARANTEED_THRESHOLD = 80;
+const int ZANMATO_IMPOSSIBLE_THRESHOLD = 16;
+const int ZANMATO_MAX_ROLL = 64;
+
+// Function prototypes
+int getUserInput(const std::string& prompt, int minValue, int maxValue);
+int calculateZanmatoChance(int yc, int gil, int od, int zl);
 
 int main() {
-	int yc; //this value is Yojimbo's Compatibility level, required for first calcuation
-	int gil; //Gil paid to Yojimbo, duh
-	int pay; //this value is determined by how much Gil you've paid to Yojimbo, required for fist calcuation
-	int fc; //first calcuation
-	int od; //determines whether Zanmato's overdrive bar is full, required for second calcuation.
-	int sc; //second calcuation
-	int zl; //determines enemy's Zanmato level, used for third calcuation.
-	int tc; //third calcuation
-	double a = 0; //final calcuation should you not hit guaranteed Zanmato
-	std::cout << "Welcome to my Zanmato Hit Probablity Calcuator in C++! This version of Calcuator is made with assumption that you've picked \"To defeat the most powerful of enemies.\" option. For love of the God \n";
-	std::cout << "\nFirst, what is Yojimbo's compatibility value (you need to either count or use a save editor to find out)? It's something between 0-255 = ";
-	std::cin >> yc;
-	while (yc <= -1 || 256 <= yc) //ensures only correct value is entered for YC
-	{
-		std::cout << "This is not a correct value, please write something between 0-255 = ";
-		std::cin >> yc;
-	}
-	std::cout << "Okay, I need to know how much gil you've paid to him = ";
-	std::cin >> gil;
-	while (gil <= 0) //ditto
-	{
-		std::cout << "This is not a correct value, please write something between 1-999999999 = ";
-		std::cin >> gil;
-	}
-	pay = gilMotivation(gil); //used to find Yojimbo's motivation from your pay
-	fc = yc / 4 + pay; //first calcuation is (Yojimbo Compatibility / 4) + the motivation he gets from your payment 
-	std::cout << "Nice, now we need to check whether Yojimbo's Overdrive bar is full. If it is, type 1. Else, type any number = ";
-	std::cin >> od;
-	if (od == 1)  // Game adds 20 to the second calcuation if Yojimbo's Overdrive meter is full, giving more chance for a Zanmato
-	{
-		sc = fc + 20;
-	}
-	else
-	{
-		sc = fc;
-	}
-	std::cout << "Great! We need to determine enemy's Zanmato Level. This is something you need to look up. If it's 1, 2 or 3 then type 1, else any number you want to = ";
-	std::cin >> zl;
-	tc = thirdCalcuation(zl, sc); //IF enemy Zanmato level is 1-3, you take 80% of second calcuation, if it's 4-6 then 40%. And that is third calcuation
-	if (80 <= tc) // the final calcuation is third calcuation + a random number between 0-63. You need to hit 80 total, so even if you roll 0 at a tc value of 80 it's guaranteed Zanmato
-	{
-		std::cout << "Zanmato is guaranteed!";
-	}
-	else if (tc <= 16) // even if you roll max 63 at 16 or less, it's guaranteed NOT Zanmato.
-	{
-		std::cout << "No chance of Zanmato!";
-	}
-	else //Assume a tc of 79, we subtract this from 80 to find that we need to roll at least 1. Then we subtract 1 from 64 (0 is also possible roll, thus giving us 64 possible roll) to find how many numbers in 64 possible roll guarantees Zanmato. Then we find out 63 possible numbers (everything except 0), then divide it and multiple it by 100 to find percentage of a roll that will make the equation 80 or more.
-	{
-		a = 80 - tc;
-		a = 64 - a;
-		a = a / 64;
-		a = a * 100;
-		std::cout << "Chance of Zanmato is = " << a;
-	}
-	std::cout << std::endl;
-	system("pause");
+    int yc = getUserInput("First, what is Yojimbo's compatibility value (0-255)? ", MIN_YC, MAX_YC);
+    int gil = getUserInput("Okay, I need to know how much gil you've paid to him (1-999999999): ", MIN_GIL, MAX_GIL);
+
+    int pay = gilMotivation(gil);
+    int fc = yc / 4 + pay;
+
+    int od = getUserInput("Great! We need to check whether Yojimbo's Overdrive bar is full. If it is, type 1. Else, type any number: ", 0, 1);
+    int sc = fc + (od == 1 ? 20 : 0);
+
+    int zl = getUserInput("Now, we need to determine the enemy's Zanmato Level. If it's 1, 2, or 3, then type 1. Otherwise, type any number: ", MIN_ZL, MAX_ZL);
+
+    int zanmatoChance = calculateZanmatoChance(yc, gil, od, zl);
+
+    if (zanmatoChance >= ZANMATO_GUARANTEED_THRESHOLD) {
+        std::cout << "Zanmato is guaranteed!";
+    }
+    else if (zanmatoChance <= ZANMATO_IMPOSSIBLE_THRESHOLD) {
+        std::cout << "No chance of Zanmato!";
+    }
+    else {
+        std::cout << "Chance of Zanmato is = " << zanmatoChance << "%";
+    }
+
+    std::cout << std::endl;
+    system("pause");
+}
+
+// Function to get validated user input
+int getUserInput(const std::string& prompt, int minValue, int maxValue) {
+    int input;
+    do {
+        std::cout << prompt;
+        std::cin >> input;
+        if (std::cin.fail() || input < minValue || input > maxValue) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else {
+            break;
+        }
+    } while (true);
+    return input;
+}
+
+// Function to calculate Zanmato chance
+int calculateZanmatoChance(int yc, int gil, int od, int zl) {
+    int pay = gilMotivation(gil);
+    int fc = yc / 4 + pay;
+    int sc = fc + (od == 1 ? 20 : 0);
+    return thirdCalcuation(zl, sc);
 }
